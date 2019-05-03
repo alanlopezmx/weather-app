49effa9191a45bab5f462871eaa91e9fbbe2eec4
Hector Alan Lopez Diaz <alanlopez1995@hotmail.com>
2019-05-03 00:31:25 -0500

    Curso 02-05-2019

--- src/components/ForecastExtended.js
+++ src/components/ForecastExtended.js
@@ -1,10 +1,63 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
+import ForecastItem from './ForecastItem';
+import * as CONSTANTS from '../constants/api_url';
+import transformForecast from '../services/transformForecast';
+import './styles.css';
 
+
+/*const days = [
+    'Lunes',
+    'Martes',
+    'Miercoles',
+    'Jueves',
+    'Viernes',
+];
+
+const data = {
+    temperature: 10,
+    humidity: 10,
+    weatherState: 'normal',
+    wind: 'normal',
+}*/
 class ForecastExtended extends Component {
+
+    constructor(){
+        super();
+        this.state = { forecastData: null};
+    }
+
+    componentDidMount(){
+        const url_forecast = `${CONSTANTS.url_base_weather_forecast}?q=${this.props.city}&appid=${CONSTANTS.api_key}`;
+
+        fetch(url_forecast)
+        .then( data =>(data.json())
+        .then( weather_data => {
+            console.log(weather_data);
+            const forecastData = transformForecast(weather_data);
+            this.setState({ forecastData })
+        })
+        );
+    }
+
+    renderForecastItemDays(){
+        return (<h1>Render items</h1>);
+        //return days.map(day => <ForecastItem weekDay={day} hour={10} data={data}></ForecastItem>)
+    }
+    renderProgress(){
+        return (<h3>Cargando pronostico extendido</h3>);
+    }
+
+
     render(){
         const {city} = this.props;
-        return (<div>Pronostico extendido para {city}</div>)
+        const { forecastData } = this.state;
+        return (
+        <div>
+            <h2 className='forecast-title'>Pronostico extendido para {city}</h2>
+            {forecastData ? this.renderForecastItemDays() : this.renderProgress()}
+        </div>
+        );
     }
 }
 
--- src/constants/api_url.js
+++ src/constants/api_url.js
@@ -1,3 +1,4 @@
 
 export const api_key = "5be96d5810de63a33a94ced791487a90";
 export const url_base_weather = "http://api.openweathermap.org/data/2.5/weather";
+export const url_base_weather_forecast = "http://api.openweathermap.org/data/2.5/forecast";
